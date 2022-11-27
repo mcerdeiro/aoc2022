ecbf6a9fb190525bc72bbef37268f8252bdc003d
Mariano Cerdeiro <mcerdeiro@gmail.com>
2022-11-27 20:17:36 +0100

    day 1 template

---
+++ 01/day01.py
@@ -0,0 +1,17 @@
+import collections
+import itertools
+
+p1 = p2 = 0
+
+lines = open('in.in').read().splitlines()
+
+vals = [int(x) for x in lines]
+for line in lines:
+    pass
+
+
+
+print(vals)
+
+print('part1', p1)
+print('part2', p2)
