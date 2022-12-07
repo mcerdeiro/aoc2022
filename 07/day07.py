ea5f25066dffd472dd01f4edbe0becd81f49cc92
Mariano Cerdeiro <mcerdeiro@gmail.com>
2022-12-07 20:58:40 +0100

    Day 7

---
+++ 07/day07.py
@@ -0,0 +1,77 @@
+import collections
+import itertools
+from os.path import exists
+
+p1 = 0
+
+file = 'in.in'
+if not exists(file):
+    file = '2022/07/' + file
+
+lines = open(file).read().splitlines()
+
+ROOT = []
+i = 0
+running = False
+location = []
+
+# parse the input
+while i < len(lines):
+    if '$ ' in lines[i]:
+        cmds = lines[i].split()
+        assert(cmds[0] == '$')
+        if cmds[1] == 'cd':
+            if cmds[2] == '/':
+                location = []
+            elif cmds[2] == '..':
+                location.pop()
+            else:
+                location.append(cmds[2])
+        elif cmds[1] == 'ls':
+            pass
+            contains = dict()
+            running = True
+    else:
+        assert(running == True)
+        sizeType, name = lines[i].split()
+        if 'dir' != sizeType:
+            sizeType = int(sizeType)
+        contains[name] = sizeType
+        if sizeType == 'dir':
+            ROOT.append(('/'.join(location) + '/' + name + '/', 0))
+        else:
+            ROOT.append(('/'.join(location) + '/' + name, int(sizeType)))
+
+    i += 1
+
+#  calculate the size and update the nodes
+for i, v in enumerate(ROOT):
+    if v[1] == 0:
+        size = 0
+        for v2 in ROOT:
+            if v2 == v:
+                continue
+            if v2[0].startswith(v[0]) and not v2[0].endswith('/'):
+                size += v2[1]
+        ROOT[i] = (v[0], size)
+
+
+totalusedspace = 0
+for d in ROOT:
+    if d[0].endswith('/'):
+        if d[1] <= 100000:
+            p1 += d[1]
+    else:
+        totalusedspace += d[1]
+
+print('Part1:', p1)
+
+goal = 30000000-(70000000-totalusedspace)
+current = 100000000000
+for d in ROOT:
+    if d[0].endswith('/'):
+       if d[1] > goal and d[1] < current:
+             current = d[1]
+
+print('Part2:', current)
+
